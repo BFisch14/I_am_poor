375bc1b4de2e5755bdbda92e8b23e8d3648fcb2e
BFisch14 <bradfisch142010@gmail.com>
2020-05-11 13:59:25 -0700

    Added image

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
     home: Scaffold(
+      body: Image.network('https://img.freepik.com/free-photo/dirty-hands-homeless-poor-man-with-empty-wallet-modern-capitalism-society_140289-3.jpg?size=626&ext=jpg'),
       backgroundColor: Colors.grey[500],
       appBar: AppBar(
         title: new Text("I am Poor", style: TextStyle( 
